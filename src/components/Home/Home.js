4bffeaf9753727fb9eacac6000711502d0ad6b87
muhammadsajit <sajit.du.niter@gmail.com>
2022-04-04 21:57:58 +0600

    button added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
                   <h1>Rolex Watch choices are <span> best choice</span> </h1>
                   <h1>For everyone...</h1>
                   <p>Rolex offers a wide assortment of Oyster Perpetual and Cellini watches to suit any wrist. Discover the broad selection of Rolex watches to find a perfect combination of style and functionality.</p>
+                  <button className='btn'>Live Demo</button>
                 </div>
                 <div className='half-width'>
                 <img className='image'src={watch} alt="" />
